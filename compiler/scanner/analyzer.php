8c65065c2babd0559b2bc62aaaace6a953904c19
erwintobing15 <erwintobing15@gmail.com>
2021-07-22 13:25:31 +0700

    Make method split_string in class analyzer """ method split_string used to split a string or word that contain symbol other than constant and identifier ex : #include become # and include """

--- compiler/scanner/analyzer.php
+++ compiler/scanner/analyzer.php
@@ -35,6 +35,57 @@ class Analyzer {
         return FALSE;
     }
 
+    // split string that contain symbol
+    function split_string($str,$identifiers,$constants,$special_symbols) {
+
+        // initialize array as an output
+        $str_result = [];
+
+        $splitted_str = str_split($str);
+
+        // initialize temp string
+        $temp_str = $splitted_str[0];
+        for ($i = 1; $i < sizeof($splitted_str); $i++)
+        {
+            // check if input string is identifier or constant
+            $is_char_before_identifier = in_array($splitted_str[$i-1], $identifiers, TRUE);
+            $is_char_now_identifier = in_array($splitted_str[$i], $identifiers, TRUE);
+            $is_char_before_constant = in_array($splitted_str[$i-1], $constants, TRUE);
+            $is_char_now_constant = in_array($splitted_str[$i], $constants, TRUE);
+
+            $char_before_not_symbol = ($is_char_before_identifier || $is_char_before_constant);
+            $char_now_not_symbol = ($is_char_now_identifier || $is_char_now_constant);
+
+            // concate char to new word if it is identifier or constant
+            // else if char is a symbol check if it need to be concated to other symbol
+            // else push string made to result and make a new string
+            if ($char_before_not_symbol == TRUE && $char_now_not_symbol == TRUE) {
+                $temp_str .= $splitted_str[$i];
+            }
+            else if ($char_before_not_symbol == FALSE && $char_now_not_symbol == FALSE) {
+                if (in_array($splitted_str[$i],$special_symbols,TRUE)) {
+                array_push($str_result, $temp_str);
+                $temp_str = $splitted_str[$i];
+                } else {
+                $temp_str .= $splitted_str[$i];
+                }
+            }
+            else {
+                array_push($str_result, $temp_str);
+                $temp_str = $splitted_str[$i];
+            }
+
+            // add new string to array result if loop end
+            if ($i == sizeof($splitted_str)-1) {
+                array_push($str_result, $temp_str);
+            }
+
+        }
+
+        // return splitted string as an array
+        return $str_result;
+    }
+
 }
 
 ?>
