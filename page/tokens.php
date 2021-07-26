d579c8a7d6775bb00e7f6297d87ee74ba6b0c34e
erwintobing15 <erwintobing15@gmail.com>
2021-07-26 09:15:00 +0700

    Updated token list in page tokens

--- page/tokens.php
+++ page/tokens.php
@@ -161,5 +161,38 @@
         </table>
     </div>
 
+    <h3>8. Komentar</h3>
+    <p>
+        Komentar merupakan baris yang tidak dieksekusi oleh kompilator sebagai perintah.
+        Komentar biasanya sebagai keterangan tentang kode yang ditulis. Komentar pada C++
+        diawali dengan simbol "//" untuk <i>single line</i> dan diapit oleh "/*" dan "*/"
+        untuk multi line komentar.
+    </p>
+    <p>Berikut contoh komentar pada C++ yang dapat dideteksi pada program analyzer :</p>
+    <ul>
+        <li>// berikut merupakan komentar satu bari</li>
+        <li>/* berikut merupakan contoh komentar <br>
+               yang terdiri dari lebih dari satu <br>
+               bari */ 
+        </li>
+    </ul>
+
+    <h3>9. Token yang tidak dapat dideteksi</h3>
+    <p>
+        Token yang tidak dapat dideteksi maksudnya token yang tidak dapat dibaca oleh program 
+        analyzer kategorinya. Pada kolom kategori tabel daftar token akan menunjukkan "Couldn't analyze token", 
+        dan pada pada kolom Error tabel total token akan menunjukkan jumlah token yang tidak 
+        dapat ditentukan kategorinya. 
+    </p>
+    <p>Berikut beberapa token C++ yang tidak dapat dideteksi atau salah membaca pada program analyzer sejauh ini :</p>
+    <ul>
+       <li>Simbol pointer *</li>
+       <li>Module C++ seperti std++.h</li>
+       <li>Simbol C++ seperti "\n", "\t"</li>
+       <li>Array dua dimensi seperti book[i][j]</li>
+       <li>Membaca nilai boolean true dan false sebagai string</li>
+    </ul>
+   
+
 </div>
 </div>
